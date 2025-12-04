cc5146a0726a2067028c5af954e13aa197cbe6b1
亚衣琴 <Saoutax@gmail.com>
2025-12-04 08:54:08 +0800

    feat(MGP/deleteUnused): new task

---
+++ src/mgp/deleteUnused.js
@@ -0,0 +1,47 @@
+import { MediaWikiApi } from "wiki-saikou";
+import config from "../utils/config.js";
+import GetJSON from "../utils/getJSON.js";
+import QueryCategory from "../utils/queryCats.js";
+import { CheckGlobalUsage } from "../utils/pageInfo.js";
+import FlagDelete from "../utils/flagDelete.js";
+
+const zhapi = new MediaWikiApi({
+    baseURL: config.zh.api,
+    headers: { "user-agent": config.useragent },
+});
+const cmapi = new MediaWikiApi({
+    baseURL: config.cm.api,
+    headers: { "user-agent": config.useragent },
+});
+
+(async () => {
+    console.log(`Start time: ${new Date().toISOString()}`);
+    
+    await zhapi.login(
+        config.zh.bot.name,
+        config.zh.bot.password,
+        undefined,
+        { retry: 25, noCache: true },
+    ).then(console.log);
+    const { lgusername: username } = await cmapi.login(
+        config.cm.bot.name,
+        config.cm.bot.password,
+        undefined,
+        { retry: 25, noCache: true },
+    ).then(res => {
+        console.log(res);
+        return res;
+    });
+
+    const { categories } = await new GetJSON(zhapi).get("User:SaoMikoto/Bot/config/deleteUnused.json");
+
+    const files = await new QueryCategory(cmapi).queryCat(categories, true, "file");
+
+    const usage = await new CheckGlobalUsage(cmapi).check(files);
+    const unused =  Object.keys(usage).filter(key => usage[key] === false);
+
+    const success = await new FlagDelete(cmapi).flagDelete(unused, "无使用或不再使用的文件", "无使用或不再使用的文件", username);
+    console.log(success);
+
+    console.log(`End time: ${new Date().toISOString()}`);
+})();
---
+++ src/utils/getJSON.js
@@ -0,0 +1,23 @@
+class GetJSON {
+    constructor(api) {
+        this.api = api;
+    }
+
+    /**
+     * 获取页面JSON内容
+     * @param {string} [title] 页面标题
+     * @returns {Promise<object} 返回解析后的JSON
+     */
+    async get(title) {
+        const { data: { query: { pages: [{ revisions: [{ content }] }]} } } = await this.api.get({
+            action: "query",
+            prop: "revisions",
+            rvprop: "content",
+            titles: title
+        });
+
+        return JSON.parse(content);
+    }
+}
+
+export default GetJSON;
