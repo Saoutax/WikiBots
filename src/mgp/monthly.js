1fd9adb5c4938608ec6b0ecc5cae17ea7fe0beda
亚衣琴 <Saoutax@gmail.com>
2026-01-10 18:45:45 +0800

    feat: monthly

---
+++ src/mgp/monthly.js
@@ -0,0 +1,56 @@
+import Parser from "wikiparser-node";
+import { zhapi as api, Login } from "../utils/apiLogin.js";
+import GetJSON from "../utils/getJSON.js";
+
+(async () => {
+    console.log(`Start time: ${new Date().toISOString()}`);
+
+    await new Login(api).login("zh.bot");
+
+    const { sectiontitle, summary } = await new GetJSON(api).get("User:SaoMikoto/Bot/config/monthly.json");
+
+    const {
+        data: {
+            query: {
+                pages: [
+                    {
+                        revisions: [{ content }],
+                    },
+                ],
+            },
+        },
+    } = await api.get({
+        action: "query",
+        prop: "revisions",
+        rvprop: "content",
+        titles: "萌娘百科:萌娘百科月报/订阅",
+    });
+
+    const parsed = Parser.parse(content);
+    const links = parsed.querySelectorAll("link");
+
+    const pages = [...new Set(
+        links
+            .map(item => item.name)
+            .filter(name => name.startsWith("User_talk:") && name !== "User_talk:示例用户")
+    )];
+
+    const text = "{{subst:User:SaoMikoto/Bot/config/monthly}}<span style=\"display:none\">~~~~</span>";
+
+    for (const title of pages) {
+        await api.postWithToken("csrf", {
+            action: "edit",
+            title,
+            section: "new",
+            sectiontitle,
+            text,
+            tags: "Bot",
+            summary,
+            watchlist: "nochange",
+            bot: true,
+        });
+        console.log(`Done: ${title}`);
+    }
+
+    console.log(`End time: ${new Date().toISOString()}`);
+})();
