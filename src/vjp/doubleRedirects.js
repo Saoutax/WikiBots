fe4811eabf36b83b11d09cc40edf3540dfcc906e
亚衣琴 <Saoutax@gmail.com>
2026-03-29 08:09:06 +0800

    feat: fix double redirects

---
+++ src/vjp/doubleRedirects.js
@@ -0,0 +1,44 @@
+import { vjpapi as api, Login } from "../utils/apiLogin.js";
+
+(async () => {
+    console.log(`Start time: ${new Date().toISOString()}`);
+
+    await new Login(api).login("vjp.bot");
+
+    const {
+        data: {
+            query: {
+                querypage: { results: doubleRedirects },
+            },
+        },
+    } = await api.post({
+        action: "query",
+        format: "json",
+        list: "querypage",
+        formatversion: "2",
+        qppage: "DoubleRedirects",
+        qplimit: "max",
+    });
+
+    for (const {
+        title,
+        databaseResult: { c_namespace, c_title },
+    } of doubleRedirects) {
+        const text =
+            Number(c_namespace) === 14
+                ? `{{分类重定向|${c_title}}}\n#REDIRECT [[:${c_title}]]`
+                : `#REDIRECT [[:${c_title}]]`;
+        await api.postWithToken("csrf", {
+            action: "edit",
+            title,
+            text,
+            tags: "Bot",
+            bot: true,
+            minor: true,
+            summary: "修复双重重定向",
+        });
+        console.log(`Done: ${title} → ${c_title}`);
+    }
+
+    console.log(`End time: ${new Date().toISOString()}`);
+})();
